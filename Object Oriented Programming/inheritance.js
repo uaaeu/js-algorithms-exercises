a8326aa7d632547e0214bc83e6eb215594244edc
uaaeu <yurtmusa@gmail.com>
2020-06-23 19:47:14 +0300

    inheritance

---
+++ Object Oriented Programming/inheritance.js
@@ -0,0 +1,41 @@
+//DRY => dont repeat yourrself. between 2nd and 22th lines eat method repeated.
+function Cat(name) {
+    this.name = name;
+  }
+  
+  Cat.prototype = {
+    constructor: Cat,
+    eat: function() {
+      console.log("nom nom nom");
+    }
+  };
+  
+  function Bear(name) {
+    this.name = name;
+  }
+  
+  Bear.prototype = {
+    constructor: Bear,
+    eat: function() {
+      console.log("nom nom nom");
+    }
+  };
+  
+  //to fix this create a supertype(parent) called Animal that includes eat method
+  function Animal() { }
+  
+  Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom");
+      } //eat method created on parent(supertype)
+  };
+
+  //so we can write the children more clearly
+  Cat.prototype = {
+    constructor: Cat,
+  };
+
+  Bear.prototype = {
+    constructor: Bear,
+  };  
