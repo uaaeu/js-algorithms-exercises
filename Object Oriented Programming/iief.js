932aaf9170f7b35d9f0e6956e0e8960e9f566fa5
uaaeu <yurtmusa@gmail.com>
2020-06-24 17:19:59 +0300

    use an ieff to create module

--- Object Oriented Programming/iief.js
+++ Object Oriented Programming/iief.js
@@ -8,3 +8,19 @@
   })(); //this is the arrow function type of iief
 
   //both outputs "A cozy nest is ready" immedietly
+
+  //Use an IIFE to Create a Module
+  let funModule = ( () => {
+    return {
+      isCuteMixin: (obj) => {
+        obj.isCute = () => {
+          return true;
+        };
+      },
+      singMixin: (obj) => {
+        obj.sing = () => {
+          console.log("Singing to an awesome tune");
+        };
+      }
+    }
+  })();
