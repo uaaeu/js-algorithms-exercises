9ae9331d0a949d95cdb01c629ed159c78235020b
uaaeu <yurtmusa@gmail.com>
2020-06-25 12:27:08 +0300

    creating new variable to avoid mutation

--- Functional Programming/avoid_mutations.js
+++ Functional Programming/avoid_mutations.js
@@ -3,8 +3,16 @@
 // The global variable
 var fixedValue = 4;
 
-function incrementer () {
-   return fixedValue + 1; //increased without mutated
+function incrementer() {
+  let incremented = fixedValue + 1; //created a new variable to avoid mutation
+  return incremented;
+}
+
+incrementer(); //equal to 5
+
+//or just return the value
+function incrementer() {
+  return fixedValue + 1; //increased without mutated
 }
 
 incrementer(); //equal to 5
@@ -12,7 +20,7 @@ console.log(fixedValue); //prints 4
 
 
 //or write function with argument
-function incrementer (value) {
+function incrementer(value) {
   return value + 1;
 }
 
