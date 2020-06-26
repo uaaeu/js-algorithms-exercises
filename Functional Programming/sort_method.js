38d76c548c1f40d3a2321324bcee3666726e0d6b
uaaeu <yurtmusa@gmail.com>
2020-06-26 13:23:37 +0300

    sort numbers in array

--- Functional Programming/sort_method.js
+++ Functional Programming/sort_method.js
@@ -1,8 +1,14 @@
 //Sort an Array using the sort Method
 function alphabeticalOrder(arr) {
-    // to sort an array alphabetically
-    return arr.sort((a,b) => a === b ? 0 : b < a ? 1 : -1);
-    // if we change b < a to a < b it will sort reverse z to a
-  }
-  alphabeticalOrder(["a", "d", "c", "a", "z", "g"]); //returns [ 'a', 'a', 'c', 'd', 'g', 'z' ]
-  
+  // to sort an array alphabetically
+  return arr.sort((a, b) => (a === b ? 0 : b < a ? 1 : -1));
+  // if we change b < a to a < b it will sort reverse z to a
+}
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]); //returns [ 'a', 'a', 'c', 'd', 'g', 'z' ]
+
+//we can sort numbers as well
+function ascendingOrder(arr) {
+  return arr.sort((a, b) => a - b ); //or we can sort reverse with .sort((a, b) => b - a)
+}
+ascendingOrder([1, 5, 2, 3, 4]);
+// Returns [1, 2, 3, 4, 5]
