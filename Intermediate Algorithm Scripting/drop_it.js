1c81c3e4a4457313eb77d34a200fcb4e38b04103
uaaeu <yurtmusa@gmail.com>
2020-06-28 16:06:48 +0300

    drop elements without loop

--- Intermediate Algorithm Scripting/drop_it.js
+++ Intermediate Algorithm Scripting/drop_it.js
@@ -1,7 +1,8 @@
 //Drop It
 function dropElements(arr, func) {
   for (let i = 0; i < arr.length; i++) {
-    if (func(arr[i])) { //check the elements in array which one is return true at first
+    if (func(arr[i])) {
+      //check the elements in array which one is return true at first
       return arr.slice(i); //after the first true slice array with that index
     }
   }
@@ -9,5 +10,14 @@ function dropElements(arr, func) {
 }
 
 dropElements([1, 2, 3], function (n) {
+  return n < 3;
+}); //return [1, 2, 3]
+
+//Shorter way with only slice without loop
+function dropElements(arr, func) {
+  return arr.slice(arr.findIndex(func) >= 0 ? arr.findIndex(func) : arr.length);
+}
+
+dropElements([5, 4, 1, 2, 3], function (n) {
   return n < 3;
 }); //return [1, 2, 3]
